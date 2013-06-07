98184f5387e8bef7be16eaebfa99b3371326da6b
Andrew Stamm <andrewstamm@me.com>
2013-06-07 18:01:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheAndrewStamm::Application.config.secret_key_base = '1fc377b28c60013c5b0243572e50dd26b27ef0ffab9ae051debab6c308b2dedda84b3fbc5935d32fbfa975e0a323b6c5bcbff0c58871883ce0fb7fbfb1e75b6b'
